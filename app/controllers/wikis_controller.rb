dcc10569a7cacba203e86e4028c66225a4aced84
Austin Sisson <austinsisson@gmail.com>
2014-11-23 13:43:08 -0500

    added create wikis functionality, updated application.html.haml

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,8 +1,20 @@
 class WikisController < ApplicationController
   def index
+    @user = current_user
+    @user.wikis
   end
   
   def create
+    @user = current_user
+    @wiki = @user.wikis.new(wiki_params)
+    
+    if @wiki.save
+      flash[:alert] = "Wiki saved succesfully!"
+    else
+      flash[:error] = "An error occurred, please try again."
+    end
+    
+    redirect_to @user
   end
 
   def destroy
@@ -12,5 +24,11 @@ class WikisController < ApplicationController
   end
 
   def show
+  end
+  
+  private
+  
+  def wiki_params
+    params.require(:wiki).permit(:name, :content, :private)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118031304) do
+ActiveRecord::Schema.define(version: 20141123182251) do
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20141118031304) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "content"
+    t.boolean  "private",    default: false
   end
 
 end
