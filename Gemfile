f86984109cb0f3451e46dde6aa80f86a7db60fac
Austin <austinsisson@gmail.com>
2014-12-03 02:32:39 +0000

    added seed data via faker

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.1.4'
-gem 'sqlite3'
+
 gem 'bootstrap-sass'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
@@ -15,6 +15,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 group :development do
   gem 'spring'
   gem 'better_errors'
+  gem 'sqlite3'
 end  
 
 gem 'devise'
@@ -24,5 +25,5 @@ gem 'haml-rails'
 gem 'friendly_id'
 gem 'pundit'
 gem 'html2haml'
-gem 'epic-editor-rails'
+gem 'faker'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+User.destroy_all
+Wiki.destroy_all
+
+# Create Users
+5.times do
+  user = User.new(
+    user_name:          Faker::Name.name,
+    email:              Faker::Internet.email,
+    password:           Faker::Lorem.characters(8),
+    role:               ['standard', 'premium'].sample
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Create Wikis
+10.times do
+  wiki = Wiki.create!(
+    user:              users.sample,
+    name:              Faker::Company.name,
+    content:           Faker::Lorem.paragraph(5),
+    private:           [true, false].sample
+    )
+end
